1a7559abcba5f39ce130f87f346bf56862bb1fe7
urmila1907 <urmilasangwan744@gmail.com>
2023-08-26 20:23:15 +0530

    using fake store api and other small changes

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import "./product.scss"
+import { useState,useEffect } from "react";
+
+const Product = () => {
+  const [products,setProducts] = useState([]);
+
+  useEffect(()=>{
+  fetch("https://fakestoreapi.com/products/1")
+    .then((res) => res.json())
+    .then((result) => setProducts(result));
+  },[])
+
+  return (
+    <div className="main-box">
+      <h1 className="title">Product Dashboard</h1>
+      {JSON.stringify(products)}
+      
+     
+    </div>
+  );
+}
+
+export default Product
